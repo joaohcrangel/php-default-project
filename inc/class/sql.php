6a79f8027b1c9f34c556d377d042a058f15bed8f
João Rangel <joaohcrangel@gmail.com>
2014-08-06 11:09:47 -0300

    New Sql Class

--- inc/class/sql.php
+++ inc/class/sql.php
@@ -3,14 +3,14 @@
 * Classe de Banco de Dados com conexão e métodos para cadastro, alteração e exclusão dinâmicos
 * @package Sql
 */
-class Sql{
-  
+class Sql {
+	
 	public $conn;
 	
 	private $server = 'localhost';
-	private $username = '';
-	private $password = '';
-	private $database = '';
+	private $username = 'root';
+	private $password = 'root';
+	private $database = 'mydb';
 	
 	/*********************************************************************************************************/	
 	/**
@@ -18,9 +18,11 @@ class Sql{
 	* @metodo conecta
 	*/	
 	public function conecta(){
-		
-		return $this->conn = mysqli_connect($this->server, $this->username, $this->password, $this->database);
-			
+
+		$this->conn = mysqli_connect($this->server, $this->username, $this->password, $this->database);
+
+		return $this->conn;
+
 	}
 	/*********************************************************************************************************/	
 	/**
@@ -47,11 +49,88 @@ class Sql{
 	* Método que executa qualquer instrução no banco de dados em uso
 	* @metodo query
 	*/	
-	public function query($query){
+	public function query($query, $params = array(), $multi = false){
 		
 		$this->conecta();
-		return mysqli_query($this->conn, $query);
-			
+
+		if(count($params)){
+
+			$query = str_replace('?','{?}', $query);
+
+			$query = $this->setParamsToQuery($query, $this->trataParams($params));
+
+		}
+		
+		if($_SERVER['HTTP_HOST'] === 'locahost' && isset($_GET['query-debug'])) pre($query);
+
+		try{
+			if($multi === false){				
+				$resource = mysqli_query($this->conn, $query);
+			}else{
+				$resource = mysqli_multi_query($this->conn, $query);
+			}
+		}catch(Exception $e){
+
+			 var_dump($e, debug_backtrace());
+
+		}
+		
+		if(!$resource){
+			var_dump(mysqli_error($this->conn), debug_backtrace());
+		}
+
+		return $resource;
+		
+	}
+
+	private function setParamsToQuery($query, $params = array()){
+
+		if(strpos($query, '{?}')>-1 && count($params) > 0){
+
+			$first = array_shift($params);
+			$query = preg_replace('/\{\?\}/', $first, $query, 1);
+
+			return $this->setParamsToQuery($query, $params);
+
+		}else{
+
+			return $query;
+
+		}
+
+	}
+
+	public function trataParams($params = array()){
+
+		$params_new = array();
+
+		foreach ($params as $value) {
+
+			switch(gettype($value)){
+				case 'string':
+				array_push($params_new, "'".utf8_decode($value)."'");
+				break;
+				case 'integer':
+				case 'float':
+				case 'double':
+				array_push($params_new, $value);
+				break;
+				case 'bool':
+				case 'boolean':
+				array_push($params_new, (($value)?1:0));
+				break;
+				case 'null':
+				array_push($params_new, 'NULL');
+				break;
+				default:
+				array_push($params_new, "''");
+				break;
+			}
+
+		}
+
+		return $params_new;
+
 	}
 	
 	/*********************************************************************************************************/	
@@ -59,9 +138,9 @@ class Sql{
 	* Método que retorna um registro fatiado em array
 	* @metodo query
 	*/	
-	public function select($query){
+	public function select($query, $params = array()){
 		
-		return $this->arrays($query, true);
+		return $this->arrays($query, true, $params);
 			
 	}
 	
@@ -102,13 +181,14 @@ class Sql{
 	*Cria uma variável $data do tipo array que irá armazenar o retorno dos dados obtidos pela query, antes porém ela executa um foreach para formatar os campos datetime, decimal e money no padrão americano e tirando espaços para os demais tipos. alimenta o array $data com os registros formatados em $record.
 	* @metodo arrays
 	*/	
-	public function arrays($query, $array = false){
+	public function arrays($query, $array = false, $params = array()){
+
 		
-		$a = $this->query($query);	
+		$a = $this->query($query, $params);	
 		$fields = array();
 		
 		$finfo = $a->fetch_fields();
-
+		
 	    foreach($finfo as $val){
 			
 			array_push($fields, array(

6e49c341a452004f2a367e23a098ab19e2c20637
Joao Rangel <joaohcrangel@gmail.com>
2014-10-02 22:16:25 -0300

    beta 1

--- index.php
+++ index.php
@@ -2,40 +2,4 @@
 
 require_once("inc/configuration.php");
 
-$a = new Animal();
-
-$a->andar();
-if($a->estaVivo){
-	echo "vivo!";
-}else{
-	echo "morto";
-}
-
-$b = new Cobra();
-
-
-$b->estaVivo = false;
-
-$b->andar();
-
-
-
-if($b->estaVivo){
-	echo "vivo!";
-}else{
-	echo "morto";
-}
-
-
-$sql = new Sql();
-
-$linhas = $sql->arrays("SELECT * FROM tb_pessoas ORDER BY despessoa");
-
-
-
-//$page = new Page();
-
-//$page->setTpl("index");
-
 ?>
-<meta charset="utf-8">

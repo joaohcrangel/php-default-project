f8738287f012759301ff555ef8cac470884a2f45
João Rangel <joaohcrangel@gmail.com>
2015-03-17 08:29:43 -0300

    Update configuration.php

--- inc/configuration.php
+++ inc/configuration.php
@@ -5,4 +5,7 @@ if((float)PHP_VERSION < 5.3) define("__DIR__", dirname(__FILE__));//Fixbug PHP <
 require_once("consts.php");
 require_once("functions.php");
 require_once("raintpl/inc/rain.tpl.class.php");
+
+spl_autoload_register('autoload');//Define a função autoload como uma função automatica de __autoload
+
 ?>

b52da6c67bfe68fa490bfdb020f73821db85b8ee
Joao Rangel <joaohcrangel@gmail.com>
2016-10-16 18:34:55 -0200

    ok

--- inc/class/Page.php
+++ inc/class/Page.php
@@ -1,21 +1,21 @@
 <?php
 class Page {
-  	
+
 	private $language;
 	private $Tpl;
-  	
+
 	public $options = array(
 		"header"=>true,
 		"footer"=>true,
 		"data"=>array(
 			"body"=>array(),
 			"js"=>array(),
-			"head_title"=>"CM Stands - Montagens Promocionais",
-			"meta_description"=>"Na CM Stands Atendemos nossos clientes em todas as suas necessidades em estandes, da criação à execução, numa gama imensa de opções, em produtos e serviços.",
+			"head_title"=>"Título do Site",
+			"meta_description"=>"Descrição",
 			"meta_author"=>"João Rangel"
 		)
 	);
- 
+
 	public function __construct($options = array()){
 
 		$rootdir = PATH;
@@ -36,15 +36,15 @@ class Page {
 
 		$tpl = $this->getTpl();
 		$this->options = $options;
- 
+
 		if (gettype($this->options['data'])=='array') {
 			foreach($this->options['data'] as $key=>$val){
 				$tpl->assign($key, $val);
 			}
 		}
- 
+
 		if ($options['header'] === true) $tpl->draw("header", false);
- 
+
 	}
 
 	public function getString($name){
@@ -52,40 +52,40 @@ class Page {
 		return $this->language->getString($name);
 
 	}
- 
+
 	public function __destruct(){
- 
+
 		$tpl = $this->getTpl();
- 
+
 		if(gettype($this->options['data'])=='array'){
 			foreach($this->options['data'] as $key=>$val){
 				$tpl->assign($key, $val);
 			}
 		}
- 
+
 		if ($this->options['footer'] === true) $tpl->draw("footer", false);
- 
+
 	}
- 
+
 	public function setTpl($tplname, $data = array(), $returnHTML = false){
- 
+
 		$tpl = $this->getTpl();
- 
+
 		if(gettype($data)=='array'){
 			foreach($data as $key=>$val){
 				$tpl->assign($key, $val);
 			}
 		}
- 
+
 		return $tpl->draw($tplname, $returnHTML);
- 
+
 	}
-  
+
 	public function getTpl(){
- 
+
 		return ($this->Tpl)?$this->Tpl:$this->Tpl = new RainTPL;
- 
+
 	}
- 
+
 }
 ?>

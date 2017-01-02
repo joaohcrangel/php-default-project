6adcda11b69d0815903a83e4c67a0eb9b5d94fcb
Ronaldo braz <carocabraz@gmail.com>
2017-01-02 11:48:53 -0200

    layout pessoas lista ok

--- inc/consts.php
+++ inc/consts.php
@@ -25,7 +25,7 @@ define('DB_NAME', 'database_name');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');

a6e6c5e420538f7358012b726000ff5a0bae86bc
Ronaldo braz <carocabraz@gmail.com>
2017-02-08 16:36:41 -0200

    tela pessoas tipos ok

--- modules/pessoatipo.php
+++ modules/pessoatipo.php
@@ -29,19 +29,11 @@ $app->delete("/pessoas-tipos/:idpessoatipo", function($idpessoatipo){
 
 	Permissao::checkSession(Permissao::ADMIN, true);
 
-	if(!(int)$idpessoatipo){
-		throw new Exception("Pessoa não informada", 400);		
-	}
-
-	if ((int)$idpessoatipo === 1) {
-		throw new Exception("Não é possível excluir o cadastro root.", 400);
+	if(!(int)$idpessoatipo > 0){
+		throw new Exception("Tipo de pessoa não informada.", 400);		
 	}
 
 	$pessoa = new PessoaTipo((int)$idpessoatipo);
-
-	if(!(int)$pessoa->idpessoatipo() > 0){
-		throw new Exception("Pessoa não encontrada", 404);		
-	}
 
 	$pessoa->remove();
 

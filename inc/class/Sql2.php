d7cef82e125005c1dd7335d5d77979ce65b31e82
João Rangel <joaohcrangel@gmail.com>
2017-02-13 19:25:24 -0200

    ok

--- inc/class/Sql2.php
+++ inc/class/Sql2.php
@@ -1,22 +1,9 @@
 <?php 
 
-<<<<<<< HEAD
 class Sql extends PDO {
 
 	private $conn;
 	private $utf8 = true;
-=======
-class Sql {
-
-	private $conn;
-
-	private $type = DB_TYPE;
-
-	private $server = DB_HOST;
-	private $username = DB_USER;
-	private $password = DB_PASSWORD;
-	private $database = DB_NAME;
->>>>>>> efc0d07f6939fda78f0d791e9fdcfbdf77684b36
 
 	public function __construct()
 	{
@@ -26,68 +13,6 @@ class Sql {
     	$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     	$this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
 
-<<<<<<< HEAD
-	}
-
-	public function query($rawQuery, $params = array()):array
-	{
-
-		$sth = $this->conn->prepare($rawQuery);
-
-		$sth->execute($params);
-
-		return $this->getArrayRows($sth);
-
-	}
-
-	public function getArrayRows($statement):array
-	{
-
-		$data = $statement->fetchAll();
-
-		foreach ($data as &$row) {
-			
-			foreach ($row as $key => $value) {
-				if (is_numeric($key)) unset($row[$key]);
-			}
-
-			foreach ($fields as $f) {
-			
-				switch ($f['type']) {
-
-					case 'NEWDECIMAL':
-					$row[$f['field']] = (float)$row[$f['field']];
-					$row['des'.$f['field']] = number_format((float)$row[$f['field']], 2, ',', '.');
-					break;
-
-					case 'LONG':
-					case 'INTEGER':
-					$row[$f['field']] = (int)$row[$f['field']];
-					break;
-
-					case 'BIT':
-					$row[$f['field']] = (bool)$row[$f['field']];
-					break;
-
-					case 'TIMESTAMP':
-					$row[$f['field']] = $row[$f['field']];
-					$row['des'.$f['field']] = date("d/m/Y", strtotime($row[$f['field']]));
-					$row['ts'.$f['field']] = strtotime($row[$f['field']]);
-					break;
-
-					case 'VAR_STRING':
-					case 'STRING':
-					$row[$f['field']] = ($this->utf8 === true)?trim(utf8_encode(trim($row[$f['field']]))):trim($row[$f['field']]);
-					break;
-
-				}
-
-			}
-
-		}
-
-		return $data;
-=======
 		return $this->conn;
 
 	}
@@ -181,14 +106,9 @@ class Sql {
 			array_push($rows, $row["Tables_in_$database"]);
 		}
 		return $rows;
->>>>>>> efc0d07f6939fda78f0d791e9fdcfbdf77684b36
 
 	}
 
 }
 
-<<<<<<< HEAD
 ?>
-=======
- ?>
->>>>>>> efc0d07f6939fda78f0d791e9fdcfbdf77684b36

b73090ed0979e5aba80142db154274b08639279d
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-03-01 18:01:03 -0300

    mapa de lugares e configuração :+1:

--- modules/admin-install.php
+++ modules/admin-install.php
@@ -3028,6 +3028,14 @@ $app->get("/install-admin/sql/configuracoes/inserts", function(){
 	));
 	$uploadMimes->save();
 
+	$googleMapKey = new Configuracao(array(
+		'desconfiguracao'=>$lang->getString('config_google_map_key'),
+		'desvalor'=>$lang->getString('google_map_key'),
+		'idconfiguracaotipo'=>$texto->getidconfiguracaotipo(),
+		'desdescricao'=>$lang->getString('config_google_map_key_description')
+	));
+	$googleMapKey->save();
+
 	echo success();
 
 });

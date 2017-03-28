4b332640285d8584986fa9807bb5d9c0de33a4b3
João Rangel <joaohcrangel@gmail.com>
2017-03-28 10:49:36 -0300

    productstypes

--- modules/products.php
+++ modules/products.php
@@ -227,7 +227,7 @@ $app->get("/products/types", function(){
     $pagination = new Pagination(
         $query,
         array(),
-        "ProdutosTypes",
+        "ProductsTypes",
         $itemsPerPage
     );
 

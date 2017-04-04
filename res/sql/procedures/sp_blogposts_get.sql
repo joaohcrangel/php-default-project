fb43d78e4e55237d137ec55061d7ea931b935a07
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-04-04 15:55:18 -0300

    blog post file path ok

--- res/sql/procedures/sp_blogposts_get.sql
+++ res/sql/procedures/sp_blogposts_get.sql
@@ -3,7 +3,7 @@ pidpost INT
 )
 BEGIN
 
-    SELECT a.*, b.desurl, c.desdirectory FROM tb_blogposts a
+    SELECT a.*, b.desurl, c.desdirectory, c.desfile, c.desextension FROM tb_blogposts a
     	INNER JOIN tb_urls b ON a.idurl = b.idurl
         LEFT JOIN tb_files c ON a.idcover = c.idfile
     WHERE idpost = pidpost;

ce37b745b2e0d216723c77368c0a9d0f633d386f
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-04-17 17:47:07 -0300

    ook

--- modules/blog-public.php
+++ modules/blog-public.php
@@ -1,103 +1,5 @@
 <?php
 
-$app->get("/blog/:desurl", function($desurl){
-
-	$sql = new Sql();
-
-	$data = $sql->query("CALL sp_blogpostbyurl_get(?);", array(
-		$desurl
-	));
-
-	if(isset($data[0])){
-
-		$post = new BlogPost($data[0]);
-
-		$categories = BlogCategories::listAll()->getFields();
-
-		$page = new Page();
-
-		$page->setTpl("blog-post", array(
-			"post"=>$post->getFields(),
-			"categories"=>$categories
-		));
-
-	}
-
-});
-
-$app->get("/public/blog-posts", function(){
-
-	$page = (int)get("page");
-	$itemsPerPage = (int)get("limit");
-
-	$where = array();
-
-	array_push($where, "a.dtpublished <= NOW()");
-
-	if(get("destitle") != ""){
-		array_push($where, "a.destitle LIKE '%".utf8_encode(get("destitle"))."%'");
-	}
-
-	if(get("desauthor") != ""){
-		array_push($where, "b.desauthor LIKE '%".utf8_encode(get("desauthor"))."%'");
-	}
-
-	if(get("dtpublished") != ""){
-		array_push($where, "a.dtpublished = '".get("dtpublished")."'");
-	}
-
-	if(get("idsCategories")){
-		array_push($where, "c.idcategory IN(".get("idcategory").")");
-	}
-
-	if(get("idsTags")){
-		array_push($where, "d.idtag IN(".get("idtag").")");
-	}
-
-	if(count($where) > 0){
-		$where = " WHERE ".implode(" AND ", $where)."";
-	}else{
-		$where = "";
-	}
-
-	$query = "
-		SELECT SQL_CALC_FOUND_ROWS a.*, b.desauthor, f.desurl, CONCAT(e.desdirectory, e.desfile, '.', e.desextension) AS descover, 
-			(
-				SELECT COUNT(idcomment) FROM tb_blogcomments WHERE idpost = a.idpost
-			) AS nrcomments,
-			(
-				SELECT GROUP_CONCAT(b.destag)
-				FROM tb_blogpoststags a
-				LEFT JOIN tb_blogtags b ON a.idtag = b.idtag
-				WHERE idpost = a.idpost
-			) AS destags FROM tb_blogposts a
-			INNER JOIN tb_blogauthors b ON a.idauthor = b.idauthor
-			LEFT JOIN tb_blogpostscategories c ON a.idpost = c.idpost
-			LEFT JOIN tb_blogpoststags d ON a.idpost = d.idpost
-            LEFT JOIN tb_files e ON a.idcover = e.idfile
-            LEFT JOIN tb_urls f ON a.idurl = f.idurl
-            LEFT JOIN tb_blogcomments g ON a.idpost = g.idpost
-		".$where." GROUP BY a.idpost LIMIT ?, ?;
-	";
-
-	$pagination = new Pagination(
-		$query,
-		array(),
-		"BlogPosts",
-		$itemsPerPage
-	);
-
-	$posts = $pagination->getPage($page);
-
-	echo success(array(
-		"data"=>$posts->getFields(),
-		"total"=>$pagination->getTotal(),
-		"currentPage"=>$page,
-		"itemsPerPage"=>$itemsPerPage
-	));
-
-});
-
 $app->get("/public/blog-categories", function(){
 
 	echo success(array("data"=>BlogCategories::listAll()->getFields()));
@@ -168,6 +70,98 @@ $app->get("/blog/categories/:desurl", function($desurl){
 			"currentPage"=>$page,
 			"total"=>$pagination->getTotal(),
 			"itemsPerPage"=>$itemsPerPage,
+			"categories"=>$categories
+		));
+
+	}
+
+});
+
+$app->get("/blog/search", function(){
+
+	$page = (int)get("page");
+	$itemsPerPage = (int)get("limit");
+
+	$where = array();
+
+	array_push($where, "a.destitle LIKE '%".utf8_encode(get("despost"))."%'");
+
+	if(count($where) > 0){
+		$where = " WHERE ".implode(" AND ", $where);
+	}else{
+		$where = "";
+	}
+
+	$query = "
+		SELECT SQL_CALC_FOUND_ROWS a.*, b.desauthor, f.desurl, CONCAT(e.desdirectory, e.desfile, '.', e.desextension) AS descover, 
+			(
+				SELECT COUNT(idcomment) FROM tb_blogcomments WHERE idpost = a.idpost
+			) AS nrcomments,
+			(
+				SELECT GROUP_CONCAT(b.destag)
+				FROM tb_blogpoststags a
+				LEFT JOIN tb_blogtags b ON a.idtag = b.idtag
+				WHERE idpost = a.idpost
+			) AS destags FROM tb_blogposts a
+			INNER JOIN tb_blogauthors b ON a.idauthor = b.idauthor
+			LEFT JOIN tb_blogpostscategories c ON a.idpost = c.idpost
+			LEFT JOIN tb_blogpoststags d ON a.idpost = d.idpost
+            LEFT JOIN tb_files e ON a.idcover = e.idfile
+            LEFT JOIN tb_urls f ON a.idurl = f.idurl
+            LEFT JOIN tb_blogcomments g ON a.idpost = g.idpost
+        ".$where." GROUP BY a.idpost LIMIT ?, ?;
+	";
+
+	$pagination = new Pagination(
+		$query,
+		array(),
+		"BlogPosts",
+		$itemsPerPage
+	);
+
+	$posts = $pagination->getPage($page);
+
+	// foreach ($posts->getFields() as $post) {
+	
+	// 	foreach ($post as $key => $value) {
+			
+	// 		$post['desdtpublished'] = strftime("%d de %B de %Y", strtotime($post['desdtpublished']));
+
+	// 	}
+
+	// }
+
+	$page = new Page();
+
+	$page->setTpl("blog-search", array(
+		"posts"=>$posts->getFields(),
+		"currentPage"=>$page,
+		"total"=>$pagination->getTotal(),
+		"itemsPerPage"=>$itemsPerPage,
+		"get"=>$_GET,
+		"categories"=>BlogCategories::listAll()->getFields()
+	));
+
+});
+
+$app->get("/blog/:desurl", function($desurl){
+
+	$sql = new Sql();
+
+	$data = $sql->query("CALL sp_blogpostbyurl_get(?);", array(
+		$desurl
+	));
+
+	if(isset($data[0])){
+
+		$post = new BlogPost($data[0]);
+
+		$categories = BlogCategories::listAll()->getFields();
+
+		$page = new Page();
+
+		$page->setTpl("blog-post", array(
+			"post"=>$post->getFields(),
 			"categories"=>$categories
 		));
 

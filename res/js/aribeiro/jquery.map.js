d1a537ab8064f6da2c8874bb9520e1461343618c
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-04-25 16:15:45 -0300

    map-plugin

--- res/js/aribeiro/jquery.map.js
+++ res/js/aribeiro/jquery.map.js
@@ -5,62 +5,69 @@
 		map:function(options){
 
 			var defaults = {
-				id: ""
+				id: "",
+				mapKey:""
 			};
 
 			var o = $.extend({}, defaults, options);
 
-			if(!o.id) console.error("Informe o ID do lugar");
+			if(!o.id){
+				console.error("Informe o ID do lugar");
+			}else if(!o.mapKey){
+				console.error("Informe a chave do mapa");
+			}else{
 
-			return this.each(function(){
+				return this.each(function(){
 
-				var $div = $(this);
+					var $div = $(this);
 
-				console.log($div);
+					rest({
+						url:PATH+"/public/places/"+o.id,
+						success:function(r){
 
-				rest({
-					url:PATH+"/public/places/"+o.id,
-					success:function(r){
+							var data = r.data;
 
-						var data = r.data;
+							console.log(data);
 
-						console.log(data);
+							window.initMap = function(){
 
-						function initMap(){
+								var position = {
+									lat: data.vllatitude,
+									lng: data.vllongitude
+								};
 
-							var position = {
-								lat: data.vllatitude,
-								lng: data.vllongitude
-							};
+								var zoom = data.nrzoom;
 
-							var zoom = data.nrzoom;
+								var map = new google.maps.Map(document.getElementById(""+$div.context.id+""), {
+									center:{lat: position.lat, lng: position.lng},
+									zoom:zoom
+								});
 
-							var map = new google.maps.Map($div, {
-								center:{lat: position.lat, lng: position.lng},
-								zoom:zoom
-							});
+								var marker = new google.maps.Marker({
+									position:position,
+									map:map,
+									draggable:true
+								});
 
-							var marker = new google.maps.Marker({
-								position:position,
-								map:map,
-								draggable:true
-							});
+								marker.setMap(map);							
 
-							marker.setMap(map);							
+								google.maps.event.addListener(map, "rightclick", function(e){
+							    	marker.setPosition(e.latLng);
+								});
 
-							google.maps.event.addListener(map, "rightclick", function(e){
-						    	marker.setPosition(e.latLng);
-							});
+							}
 
+							$("body").append('<script async defer src="https://maps.googleapis.com/maps/api/js?key='+o.mapKey+'&callback=initMap"></script>');
+
+						},
+						failure:function(r){
+							System.showError(r);
 						}
+					});
 
-					},
-					failure:function(r){
-						System.showError(r);
-					}
 				});
 
-			});
+			}
 
 		}
 
